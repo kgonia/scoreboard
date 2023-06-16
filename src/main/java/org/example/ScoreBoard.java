afdf7a6a4701710ab72198dfd434c163501fd087
Krzysztof G <krz.gonia@gmail.com>
2023-06-16 13:34:43 +0200

    changes in ScoreBoard impl, HashMap instead of Set, updateScore method & tests, dependency for parametrized tests

--- src/main/java/org/example/ScoreBoard.java
+++ src/main/java/org/example/ScoreBoard.java
@@ -1,14 +1,17 @@
 package org.example;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Optional;
 
 public class ScoreBoard {
 
-    private final Set<Match> matches = new HashSet<>();
+    static final String NEGATIVE_SCORES_ERROR_MESSAGE = "scores can't be less than zero";
+
+    private final HashMap<Match, Match> matches = new HashMap<>();
 
     public boolean addMatch(String homeTeam, String awayTeam){
-        return matches.add(new Match(homeTeam, awayTeam));
+        Match match = new Match(homeTeam, awayTeam);
+        return matches.putIfAbsent(match, match)==null;
     }
 
     public int ongoingMatches(){
@@ -16,8 +19,21 @@ public class ScoreBoard {
     }
 
     public boolean finishMatch(String homeTeam, String awayTeam) {
-        return matches.remove(new Match(homeTeam, awayTeam));
+        return matches.remove(new Match(homeTeam, awayTeam))!=null;
     }
 
+    public boolean updateScore(String homeTeam, String awayTeam, int homeTeamScore, int awayTeamScore) {
+        if(homeTeamScore < 0 || awayTeamScore < 0){
+            throw new IllegalArgumentException(NEGATIVE_SCORES_ERROR_MESSAGE);
+        }
+
+        return Optional
+                .ofNullable(matches.get(new Match(homeTeam, awayTeam)))
+                .map(match -> {
+                    match.setHomeTeamScore(homeTeamScore);
+                    match.setHomeTeamScore(awayTeamScore);
+                    return match;
+                }).isPresent();
+    }
 
 }
--- src/test/java/org/example/ScoreBoardTests.java
+++ src/test/java/org/example/ScoreBoardTests.java
@@ -1,20 +1,25 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.example.ScoreBoard.NEGATIVE_SCORES_ERROR_MESSAGE;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ScoreBoardTests {
 
+    String homeTeam = "home";
+    String awayTeam = "away";
+
     @Test
     void WhenNewMatchIsAdded_ShouldReturnTrue() {
         // given
         ScoreBoard scoreBoard = new ScoreBoard();
-        String homeTeam = "home";
-        String awayTeam = "away";
 
         // when match is added
         boolean result = scoreBoard.addMatch(homeTeam, awayTeam);
@@ -28,8 +33,6 @@ public class ScoreBoardTests {
     void WhenDuplicatedMatchIsAdded_nothingChanges() {
         // given
         ScoreBoard scoreBoard = new ScoreBoard();
-        String homeTeam = "home";
-        String awayTeam = "away";
 
         // when match is added
         scoreBoard.addMatch(homeTeam, awayTeam);
@@ -44,8 +47,6 @@ public class ScoreBoardTests {
     void WhenMatchIsRemoved_ongoingMatchesShouldDecrease() {
         // given
         ScoreBoard scoreBoard = new ScoreBoard();
-        String homeTeam = "home";
-        String awayTeam = "away";
 
         // when match is added
         scoreBoard.addMatch(homeTeam, awayTeam);
@@ -64,8 +65,6 @@ public class ScoreBoardTests {
     void WhenNonExistingMatchIsRemoved_nothingChanges() {
         // given
         ScoreBoard scoreBoard = new ScoreBoard();
-        String homeTeam = "home";
-        String awayTeam = "away";
 
         int ongoingMatchesBeforeRemoval = scoreBoard.ongoingMatches();
         // when
@@ -75,5 +74,46 @@ public class ScoreBoardTests {
         assertFalse(result);
         // ongoing matches is reduces and equal 0
         assertThat(scoreBoard.ongoingMatches()).isEqualTo(ongoingMatchesBeforeRemoval).isEqualTo(0);
+    }
+
+    @Test
+    void WhenNonExistingMatchIsUpdate_shouldReturnTrue() {
+        // given
+        ScoreBoard scoreBoard = new ScoreBoard();
+
+        scoreBoard.addMatch(homeTeam, awayTeam);
+        scoreBoard.addMatch(homeTeam, awayTeam);
+
+        // when
+        boolean result = scoreBoard.updateScore(homeTeam, awayTeam, 0, 1);
+
+        // then
+        assertTrue(result);
+    }
+
+
+    @Test
+    void WhenMatchIsUpdated_scoreIsUpdated() {
+        // given
+        ScoreBoard scoreBoard = new ScoreBoard();
+
+        // when
+        boolean result = scoreBoard.updateScore(homeTeam, awayTeam, 0, 1);
+
+        // then
+        assertFalse(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"-1,1", "3,-2", "-4,-3"})
+    void WhenScoreIsLessThanZero_throwException(int homeTeamScore, int awayTeamScore) {
+        // given
+        ScoreBoard scoreBoard = new ScoreBoard();
+
+        // when .. then
+        assertThatThrownBy(() -> {
+            scoreBoard.updateScore(homeTeam, awayTeam, homeTeamScore, awayTeamScore);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NEGATIVE_SCORES_ERROR_MESSAGE);
     }
 }
